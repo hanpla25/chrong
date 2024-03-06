074babad007654c2a5ef0e23b47162ca3111b65c
hanpla25 <hanpla25@gmail.com>
2024-03-06 14:50:24 +0900

    경로수정

--- JS/background.js
+++ JS/background.js
@@ -1,7 +1,7 @@
 const images = ["0.jpg", "1.jpg", "2.jpg"];
 const selectedImage = images[Math.floor(Math.random() * images.length)];
 
-document.body.style.backgroundImage = `url(../IMG/${selectedImage})`;
+document.body.style.backgroundImage = `url(../images/${selectedImage})`;
 document.body.style.backgroundPosition = "top";
 document.body.style.backgroundRepeat = "no-repeat";
 document.body.style.backgroundSize = "cover";
