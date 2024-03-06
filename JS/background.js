8a90098550be5050d5daaa3658d054c90c6df1a3
hanpla25 <158482801+hanpla25@users.noreply.github.com>
2024-03-06 15:10:19 +0900

    Delete JS directory

--- JS/background.js
+++
@@ -1,8 +0,0 @@
-const images = ["0.jpg", "1.jpg", "2.jpg"];
-const selectedImage = images[Math.floor(Math.random() * images.length)];
-
-document.body.style.backgroundImage = `url(../images/${selectedImage})`;
-document.body.style.backgroundPosition = "top";
-document.body.style.backgroundRepeat = "no-repeat";
-document.body.style.backgroundSize = "cover";
-document.body.style.backgroundAttachment = "fixed";
